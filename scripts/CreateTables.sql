e35c452505c24c0d68a33e9f8ce4edc18078b11e
Antonio Ponce <zponce@posteo.net>
2023-04-28 12:55:53 -0400

    Rewrote insert proc MigrateUserData to be compatible with mySQL, other changes

--- scripts/CreateTables.sql
+++ scripts/CreateTables.sql
@@ -1,6 +1,7 @@
--- Run this script to create the right tables for mundus-ts
+-- Run this script to create the right tables for mundus-ts 
+-- NOTE: This script will only work for MYSQL
 CREATE TABLE Users(username longtext, created datetime, last_modified datetime, user_id serial primary key);
 
-CREATE TABLE UserStatus (status longtext, last_modified datetime, user_id int primary key);
+CREATE TABLE UserStatus (status longtext, created datetime, last_modified datetime, user_id int, record_id int auto_increment primary key);
 
-CREATE TABLE UserInfo (url longtext, type longtext, created datetime, last_modified datetime, user_id int primary key);
+CREATE TABLE UserInfo (url longtext, type longtext, created datetime, last_modified datetime, user_id int, entry_id int serial primary key);
---
+++ scripts/MigrateUserData.sql
@@ -0,0 +1,42 @@
+DELIMITER //
+/**************************************
+* 
+*   Author: Antonio Ponce  (06/04/23)
+*   Purpose: Migrate old data from SQlite3 table 'Userlib' to appropriate tables in MySQL 
+*   Note: This is a mySQL script. Will need to be translated for use elsewhere...
+*
+*   ChangeLog:
+*   --Name----------------Date---------------------Description----------
+*   Antonio Ponce         (27/04/23)               Changed proc to be a stand-alone insert
+*
+**************************************/ 
+
+CREATE PROCEDURE MigrateUserData(
+    IN UserName VARCHAR(255), 
+    IN GitHub VARCHAR(255), 
+    IN LinkedIn VARCHAR(255), 
+    IN Website VARCHAR(255), 
+    IN Status VARCHAR(255))
+BEGIN 
+
+    SET @CreatedDate = NOW();
+    SET @LastModified = NOW();
+    
+    -- Update initial user date into Users table 
+    INSERT INTO Users (username, created, last_modified) VALUES (Username, @CreatedDate, @LastModified);
+    -- Get auto generated Userid
+    SELECT user_id FROM Users ORDER BY Created DESC LIMIT 1 INTO @UserId; 
+
+    -- Insert into UserInfo, 3 inserts here<> (website, github, linkedin)
+    IF GitHub <> '' THEN INSERT INTO UserInfo (url, type, created, last_modified, user_id) VALUES (GitHub, 'GitHub', @CreatedDate, @LastModified, @UserId);
+    END IF;  
+
+    IF LinkedIn <> '' THEN INSERT INTO UserInfo (url, type, created, last_modified, user_id) VALUES (LinkedIn, 'LinkedIn', @CreatedDate, @LastModified, @UserId);
+    END IF;  
+
+    IF Website <> '' THEN INSERT INTO UserInfo (url, type, created, last_modified, user_id) VALUES (Website, 'Website', @CreatedDate, @LastModified, @UserId);
+    END IF; 
+    -- Insert into UserStatus 
+    INSERT INTO UserStatus (status, created, last_modified, user_id) VALUES (Status, @CreatedDate, @LastModified, @UserId); 
+END 
+//
