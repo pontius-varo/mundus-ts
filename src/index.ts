69c4c62379de5b0c4fc70c0fa8f79e76953480a8
Antonio Ponce <zponce@posteo.net>
2023-05-01 22:59:02 -0400

    created getUser function, other minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,79 @@
 "use strict";
 //import fs from 'fs';
-import TelegramBot from "node-telegram-bot-api";
+import TelegramBot, { Message } from "node-telegram-bot-api";
+import mysql from 'mysql2';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const port: number = 4444;
 
-let myToken: string = "";
+// API Key
+let botToken: string;
 
+function extractEnvironmentVariable(key: keyof NodeJS.ProcessEnv): string {
+    
+    let value = process.env[key];
 
-function foo(value: string): string{
+    if(value === undefined){
+        throw `${key} is not defined! Terminating...`;
+    }
+
     return value;
-}
-
-foo('Foobar');
-
-const myFunc: Function = () => {
-    console.log("This is my function!");
 };
 
-myFunc();
+function getUserInfo(msg: Message, username: string): string {
 
-function getMyCords(pt: { x: number, y: number, area?: string}){
-    return `My X is: ${pt.x}, My Y is: ${pt.y}, and my area is ${pt.area}`
-}
+    let userInfo: string = '';
 
-// Both are valid
-getMyCords({x: 123, y: 324324});
-getMyCords({x: 12, y: 233, area: "foobar"});
+    // proc name: GetUserInfo
+    // should return a string only showing data the user has (ie only has website + github, etc)
 
-// Type alias
-
-type Person = {
-    name: string;
-    address: string;
-    number: number;
-}
-
-// interface (can be extended)
-interface Bob extends Person {
-    bobness: string;
-}
+    if(username == 'None'){
+        // use the username defined in msg 
+        // get user info from SQL by invoking a proc;
+    } else {
+        // use the username passed in the invocation
+        // get user info from SQL by invoking a proc;
+    }
 
 
-// will only accept person 'type' objects
-function showPerson(person: Person){
-    return console.log(person);
-}
+    return userInfo;
+};
+
+
+// NIRN_BOT_API_KEY
+function main(): void {
+
+    const poolConfig = {
+        host: extractEnvironmentVariable("SQL_HOST"),
+        user: extractEnvironmentVariable("SQL_USER"),
+        database: extractEnvironmentVariable("SQL_DATABASE"),
+        password: extractEnvironmentVariable("SQL_USER_PASSWORD"),
+        connectionLimit: 250,
+    }
+
+
+    let isTesting: string = extractEnvironmentVariable("TESTING");
+
+    if(isTesting == "true"){
+        botToken = extractEnvironmentVariable("NIRN_BOT_API_KEY");
+    } else {
+        botToken = extractEnvironmentVariable("PRODUCTION_BOT_API_KEY");
+    }
+
+    // Create the bot 
+    const bot: TelegramBot = new TelegramBot(botToken, { polling: true });
+
+    // getuser command 
+    bot.onText(/\/getuser (.+)/, (msg, match) => {
+
+        let response: string;
+        
+        // if match is not undefined, pass it through. Else, pass an empty string
+        response = getUserInfo(msg, match ? match[1] : 'None') ;
+        
+        // Return user data
+        bot.sendMessage(msg.chat.id, response);
+    });
+
+};
