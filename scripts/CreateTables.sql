52536f9e238f773ae1fa792842c248dfe92d7107
Antonio Ponce <zponce@posteo.net>
2023-05-04 23:59:15 -0400

    changed typo

--- scripts/CreateTables.sql
+++ scripts/CreateTables.sql
@@ -2,6 +2,6 @@
 -- NOTE: This script will only work for MYSQL
 CREATE TABLE Users(username longtext, created datetime, last_modified datetime, user_id serial primary key);
 
-CREATE TABLE UserStatus (status longtext, created datetime, last_modified datetime, user_id int, record_id int auto_increment primary key);
+CREATE TABLE UserStatus (status longtext, created datetime, last_modified datetime, user_id int, status_entry_id int auto_increment primary key);
 
-CREATE TABLE UserInfo (url longtext, type longtext, created datetime, last_modified datetime, user_id int, entry_id int serial primary key);
+CREATE TABLE UserInfo (url longtext, type longtext, created datetime, last_modified datetime, user_id int, info_entry_id int serial primary key);
